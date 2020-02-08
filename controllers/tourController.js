f99b3a077e401d8dfdbe63c13841825e5a6034f8
Vamsi Krishna <vamsi.krishna@knowledgehut.co>
2020-02-08 22:48:33 +0530

    Changed Folder structure and code to impliment MVC architecture

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,3 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
-
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -27,52 +9,46 @@ exports.checkBody = (req, res, next) => {
 };
 
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours
-    }
+    requestedAt: req.requestTime
+    // results: tours.length,
+    // data: {
+    //   tours
+    // }
   });
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+  // const id = req.params.id * 1;
 
-  const tour = tours.find(el => el.id === id);
+  // const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
+    status: 'success'
+    // data: {
+    //   tour
+    // }
   });
 };
 
 exports.createTour = (req, res) => {
   // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   err => {
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: {
+  //         tour: newTour
+  //       }
+  //     });
+  //   }
+  // );
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv');
-const app = require('./app');
 const mongoose = require('mongoose');
-dotenv.config({ path: './config.env' });
 
+// const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 ); // connection string
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -14,25 +16,7 @@ mongoose
     useFindAndModify: true,
     useUnifiedTopology: true
   })
-  .then(con => {
+  .then(() => {
     //con - connection object
-    // console.log('con', con.connections);
+    console.log('DB connection successful');
   });
-
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
