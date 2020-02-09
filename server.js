beaa7e767e1a668bb65f5b60f4b2a2a5e83c8f17
Vamsi Krishna <vamsi.krishna@knowledgehut.co>
2020-02-09 14:48:29 +0530

    feat(implimented pagination, sorting, aliasing, filtering, limiting fields): implimented pagination, sorting, aliasing
    implimented pagination, sorting, aliasing, filtering, limiting fields

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const app = require('./app');
+
+const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
